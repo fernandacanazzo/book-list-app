ff328b5e79eab46e34acab371eb3413f643d1f29
fernandacanazzo <fernandacanazzo@gmail.com>
2024-01-15 10:32:16 -0300

    Fix CORS headers on weather API

--- controllers/WeatherController.php
+++ controllers/WeatherController.php
@@ -12,6 +12,37 @@ class WeatherController extends Controller
 
     public $enableCsrfValidation = false;
 
+    public function behaviors() {
+
+        $behaviors = parent::behaviors();
+
+        $behaviors['authenticator'] = [
+            'class' => \sizeg\jwt\JwtHttpBearerAuth::class,
+            'except' => [
+                'login',
+                'refresh-token',
+                'index',
+            ],
+        ];
+
+        // remove authentication filter
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+        ];
+        
+        // re-add authentication filter
+        $behaviors['authenticator'] = $auth;
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['index'];
+
+        return $behaviors;
+
+    }
+
     public function actionIndex(){
 
        $apiKey = Yii::$app->params['apiKey'];
